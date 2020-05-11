df4be4cea148178a50ad9da7c5a0fef352748973
FabriceIRANKUNDA <irankundafabrice8@gmail.com>
2020-05-11 22:46:51 +0200

    Add comment

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 
+// Start Express
 const app = express();
 
 app.set('view engine', 'pug');
