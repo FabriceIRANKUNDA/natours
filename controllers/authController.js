fb1e644d2632a7b54bea905bbf9eacc728a60ef9
FabriceIRANKUNDA <irankundafabrice8@gmail.com>
2020-05-14 00:57:08 +0200

    Updated signUp feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,7 +43,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
   // const url = `${req.protocol}://${req.get('host')}/me`;
   // await new Email(newUser, url).sendWelcome();
   //createSendToken(newUser, 201, req, res);
-  newuser.password = undefined;
+  newUser.password = undefined;
   res.status(200).json({
     status: 'success',
     data: {
